cad3207066cf625f888b9cf218e6199db427c173
USMAN <101738023+skusman09@users.noreply.github.com>
2023-12-04 20:22:41 +0530

    changes

--- CC/InterDate.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.*;
-
-public interface InterDate extends Remote //Create an Interface and extending the properties of Remote Class (RMI)
-{
-    public String display() throws Exception;  //Create a method display()
-}
-
